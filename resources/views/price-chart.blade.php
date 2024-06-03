c4b3a990da2cf5d869fb784b656d74fcfdffbcbd
Cybernetics <mail@cybernetics.me>
2024-06-03 12:09:25 +0530

    small updates

--- resources/views/price-chart.blade.php
+++ resources/views/price-chart.blade.php
@@ -1,4 +1,6 @@
 @extends("base")
 @section("content")
-
+<script type="text/javascript">
+    document.write('<iframe width="300" height="260" src="https://liveindex.org/widget/300-dark.php" frameborder="0" hspace="0" vspace="0" scrolling="0" style="background:transparent" ></iframe>')
+</script>
 @endsection
