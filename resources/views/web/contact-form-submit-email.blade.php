c965931e564f731965abe99a74d7750e7beafa52
Cybernetics <mail@cybernetics.me>
2024-06-06 08:50:26 +0530

    small updates

--- resources/views/web/contact-form-submit-email.blade.php
+++ resources/views/web/contact-form-submit-email.blade.php
@@ -8,7 +8,7 @@
 <body>
     Dear IBT,<br />
 
-    <p>You have received new message via websiet contact form.</p><br />
+    <p>You have received new message via websiet contact form.</p>
 
     <p>Name: {{ $data->name }}</p>
     <p>Contact number: {{ $data->contact }}</p>
